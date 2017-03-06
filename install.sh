ee644d07c06abde099cadeed004878eb8b46c43a
panhongyin <iamphy@qq.com>
2017-03-06 23:03:09 +0800

    delete crudini tool to get os name

--- install.sh
+++ install.sh
@@ -5,20 +5,21 @@ if test $UID -ne 0; then
     exit 1
 fi
 
-case $(crudini --get /etc/os-release  DEFAULT ID) in
-    'ubuntu'):
+os_name=$(grep -w "NAME" /etc/os-release | awk -F '=' '{print $2}')
+case $os_name in
+    '"Ubuntu"'):
         apt install -y vim python-flake8
         cp -f ./vimrc /etc/vim/vimrc
         ;;
-    '"centos"'):
+    '"CentOS"'):
         yum install -y vim python-flake8
         /usr/bin/cp -f ./vimrc /etc/vimrc
         ;;
     *):
         echo "I don't know the OS is what ghost"
+        exit 1
         ;;
 esac
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 vim +PluginInstall +qall
