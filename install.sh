c798c3cb7adf78b4274533aaa8854ecae37125b9
panhongyin <iamphy@qq.com>
2017-03-08 14:47:32 +0800

    change mode of execution installl.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if test $UID -ne 0; then
-    echo "Sorry, I need root, piggy !!!"
-    exit 1
-fi
+#if test $UID -ne 0; then
+#    echo "Sorry, I need root, piggy !!!"
+#    exit 1
+#fi
 
 source /etc/os-release
 
@@ -12,14 +12,14 @@ process_on_ubuntu()
     packages="vim python-flake8"
 
     for pkg in $packages; do
-        apt install -y $pkg
+        sudo apt install -y $pkg
         if test $? -ne 0; then
             echo "Install $pkg fail"
             exit 1
         fi
     done
 
-    /bin/cp -f ./vimrc /etc/vim/vimrc
+    sudo /bin/cp -f ./vimrc /etc/vim/vimrc
 }
 
 process_on_centos()
@@ -27,14 +27,14 @@ process_on_centos()
     packages="vim python-flake8"
 
     for pkg in $packages; do
-        yum install -y $pkg
+        sudo yum install -y $pkg
         if test $? -ne 0; then
             echo "Install $pkg fail"
             exit 1
         fi
     done
 
-    /usr/bin/cp -f ./vimrc /etc/vimrc
+    sudo /usr/bin/cp -f ./vimrc /etc/vimrc
 }
 
 case $ID in
